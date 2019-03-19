aee3f6372dc1aab3b1c84c9c9022d420a1d4d8d0
pabloito <pradnic@itba.edu.ar>
2019-03-19 19:24:48 -0300

    IDDFS (ignores open)

--- SIA/src/gps/GPSEngine.java
+++ SIA/src/gps/GPSEngine.java
@@ -10,21 +10,22 @@ import gps.implementation.*;
 
 import javax.naming.OperationNotSupportedException;
 
+import static gps.SearchStrategy.IDDFS;
 import static gps.implementation.Direction.*;
 
 public class GPSEngine {
 
-	Queue<GPSNode> open;
-	Map<State, Integer> bestCosts;
-	Problem problem;
-	long explosionCounter;
-	boolean finished;
-	boolean failed;
-	GPSNode solutionNode;
-	Optional<Heuristic> heuristic;
+	private Queue<GPSNode> open;
+	private Map<State, Integer> bestCosts;
+	private Problem problem;
+	private long explosionCounter;
+	private boolean finished;
+	private boolean failed;
+	private GPSNode solutionNode;
+	private Heuristic heuristic;
 
 	// Use this variable in open set order.
-	protected SearchStrategy strategy;
+	private  SearchStrategy strategy;
 
 	public static void main(String args[]){
 		Heuristic heuristic1 = new HeuristicOneImpl();
@@ -67,7 +68,7 @@ public class GPSEngine {
 		bestCosts = new HashMap<>();
 		this.problem = problem;
 		this.strategy = strategy;
-		this.heuristic = Optional.of(heuristic);
+		this.heuristic = heuristic;
 		explosionCounter = 0;
 		finished = false;
 		failed = false;
@@ -76,7 +77,11 @@ public class GPSEngine {
 	public void findSolution() {
 		GPSNode rootNode = new GPSNode(problem.getInitState(), 0, null);
 		open.add(rootNode);
-		// TODO: ¿Lógica de IDDFS?
+		if(strategy==IDDFS)
+		{
+			findSolution_IDDFS(rootNode);
+			return;
+		}
 		while (open.size() <= 0) {
 			GPSNode currentNode = open.remove();
 			if (problem.isGoal(currentNode.getState())) {
@@ -89,6 +94,61 @@ public class GPSEngine {
 		}
 		failed = true;
 		finished = true;
+	}
+
+	private void findSolution_IDDFS(GPSNode rootNode) {
+		IDDFSPackage pack = new IDDFSPackage(null,true);
+		int depth =0;
+		while(pack.remaindingNodes)
+		{
+			pack= depthLimitedDFS(rootNode,depth);
+			if(pack.node!=null){
+				finished=true;
+				solutionNode=pack.node;
+				return;
+			}
+			if(!pack.remaindingNodes) {
+				finished=true;
+				failed=true;
+				return;
+			}
+			depth++;
+
+		}
+	}
+
+	private IDDFSPackage depthLimitedDFS(GPSNode node, int depth) {
+		if(depth==0){
+			if(problem.isGoal(node.getState())) {
+				return new IDDFSPackage(node, true); //El booleano no importa
+			}
+			return new IDDFSPackage(null, true);
+		}
+		boolean remaining = false;
+		Collection<GPSNode> collection = new LinkedList<>();
+		addCandidates(node,collection);
+
+		for (GPSNode childNode: collection ) {
+			IDDFSPackage pack = depthLimitedDFS(childNode, depth-1);
+			if(pack.node!=null){
+				return new IDDFSPackage(pack.node,true);
+			}
+			remaining=pack.remaindingNodes;
+		}
+		return new IDDFSPackage(null,remaining);
+
+	}
+
+	private class IDDFSPackage
+	{
+		private boolean remaindingNodes;
+		private GPSNode node;
+
+		public IDDFSPackage(GPSNode node, boolean remaindingNodes)
+		{
+			this.node=node;
+			this.remaindingNodes=remaindingNodes;
+		}
 	}
 
 	private void explode(GPSNode node) {
@@ -119,7 +179,7 @@ public class GPSEngine {
 			// TODO: ¿Cómo se agregan los nodos a open en IDDFS?
 			break;
 		case GREEDY:
-			newCandidates = new PriorityQueue<>(/* TODO: Comparator! */);
+			newCandidates = new ArrayList<>();
 			addCandidates(node, newCandidates);
 			open.addAll(newCandidates);
 			break;
@@ -154,6 +214,10 @@ public class GPSEngine {
 	private void updateBest(GPSNode node) {
 		bestCosts.put(node.getState(), node.getCost());
 	}
+
+
+
+
 
 	// GETTERS FOR THE PEOPLE!
 
