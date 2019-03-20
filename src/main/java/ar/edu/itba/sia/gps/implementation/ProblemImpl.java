16ffa3dd77d3b247e07e9c481e976aa4ba048bba
Constanza De Rienzo <cderienzo@itba.edu.ar>
2019-03-20 16:53:48 -0300

    Problem getRules

--- src/main/java/ar/edu/itba/sia/gps/implementation/ProblemImpl.java
+++ src/main/java/ar/edu/itba/sia/gps/implementation/ProblemImpl.java
@@ -3,22 +3,27 @@ package ar.edu.itba.sia.gps.implementation;
 import ar.edu.itba.sia.gps.api.Problem;
 import ar.edu.itba.sia.gps.api.Rule;
 import ar.edu.itba.sia.gps.api.State;
+import jdk.nashorn.internal.parser.JSONParser;
+import sun.plugin.javascript.JSObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProblemImpl implements Problem {
 
-    private State initState;
-    private List<Rule> rules;
 
-    public ProblemImpl(State initState, List<Rule> rules){
-        this.initState=initState;
-        this.rules=rules;
+    private int width, height;
+    private List<Square> squares;
+    private List<Changer> changers;
+    public ProblemImpl(JSObject level){
+        //parse level
+        //this.width = width;
+        //this.height = height;
+
     }
-
     @Override
     public State getInitState() {
-        return initState;
+       return new StateImpl(width, height, squares, changers);
     }
 
     @Override
@@ -28,6 +33,12 @@ public class ProblemImpl implements Problem {
 
     @Override
     public List<Rule> getRules() {
+        List<Rule> rules = new ArrayList<>();
+        StateImpl state = (StateImpl)getInitState();
+        for(Square square : state.getSquares())
+        {
+            rules.add(new RuleImpl(square));
+        }
         return rules;
     }
 
